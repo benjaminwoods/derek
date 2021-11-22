edbf6f88731e2ab6f68ee105bebfd634595960b7
Benjamin Woods <benjaminwoods@live.co.uk>
2021-11-22 21:53:08 +0000

    Add class method, Derek.tree.
    - For a specified object, obj, create a linked tree of nodes corresponding to that object.
    - Not currently testing against child nodes.

--- python/src/derek/_derek.py
+++ python/src/derek/_derek.py
@@ -1,6 +1,41 @@
 class Derek:
+    # TODO: Add docstrings
+    # TODO: Add reload method
+    # TODO: Add checkIntegrity method
+    # TODO: Add typing
+
     def __init__(self, parent=None, children=None, value=None, name=None):
         self.parent = parent
         self.children = children
         self.value = value
         self.name = name
+
+    @classmethod
+    def tree(cls, obj, parent=None, name=None):
+        """
+        Create a tree.
+        """
+        self = cls.__new__(cls)
+        children = []
+
+        if isinstance(obj, list):
+            # Make child nodes
+            children = [
+                cls.tree(item, self)
+                for item in obj
+            ]
+        elif isinstance(obj, dict):
+            # Make child nodes
+            children = [
+                cls.tree(item, self)
+                for item in obj.values()
+            ]
+        else:
+            # Not iterable
+            children = None
+
+        self.parent = parent
+        self.children = children
+        self.value = obj
+        self.name = name
+        return self
--- python/tests/derek/test__derek.py
+++ python/tests/derek/test__derek.py
@@ -1,24 +1,197 @@
-from derek import _derek
+import pytest
 
-class TestDerek:
-    def test_initialization(self):
+from derek._derek import Derek
+
+class Test_Initialization:
+    def test_defaults(self):
         """
-        Try making a Derek node.
+        Try making a Derek node, using defaults.
         """
 
-        node = _derek.Derek()
+        node = Derek()
         assert node.parent is None
         assert node.children is None
         assert node.value is None
         assert node.name is None
 
-        parent = node
+    def test_with_arguments(self):
+        """
+        Try making a Derek node, specifying arguments.
+        """
+
+        parent = Derek()
         attributes = {
             "parent": parent,
             "children": [1, 2, 3],
             "value": [1, 2, 3],
             "name": "some_node"
         }
-        node = _derek.Derek(**attributes)
+        node = Derek(**attributes)
         for k,v in attributes.items():
             assert getattr(node, k) == v
+
+class TreeBaseTest:
+    @pytest.fixture(scope="function")
+    def tree(self):
+        def _inner(*args, **kwargs):
+            self.derek = Derek.tree(*args, **kwargs)
+        yield _inner
+
+        if hasattr('derek', self):
+            del self.derek
+
+    def check(self, obj, **kwargs):
+        node = Derek.tree(obj, **kwargs)
+        if any(map(lambda t: isinstance(obj, t), [list, dict])):
+            # If obj is a subclass of any out of [list, dict]
+            assert node.value is obj
+
+        if 'name' in kwargs:
+            assert node.name == kwargs['name']
+        elif 'parent' in kwargs:
+            assert node.parent is kwargs['parent']
+
+        # TODO: check child nodes
+
+class Test_Tree(TreeBaseTest):
+    def test_name(self):
+        """
+        Try making a Derek tree from an integer,
+        specifying the name of the root node.
+        """
+        # TODO: upgrade to monkey test
+
+        args = (3141,)
+        kwargs = {
+            "name": "some_node_name"
+        }
+
+        self.check(*args, **kwargs)
+
+    def test_no_name(self):
+        """
+        Try making a Derek tree from an integer,
+        without specifying the name of the root node.
+        """
+        # TODO: upgrade to monkey test
+
+        args = (3141,)
+
+        self.check(*args)
+
+    def test_parent(self):
+        """
+        Try making a Derek subtree from an integer,
+        specifying the parent node.
+        """
+
+        args = (3141,)
+        kwargs = {
+            "parent": Derek()
+        }
+
+        self.check(*args, **kwargs)
+
+    def test_no_parent(self):
+        """
+        Try making a Derek tree from an integer,
+        without specifying the name of the root node.
+        """
+
+        args = (3141,)
+
+        self.check(*args)
+
+    def test_non_iterable(self):
+        """
+        Try making a Derek tree from a non-iterable object,
+        without specifying the name of the root node.
+        """
+        # TODO: monkey testing
+
+        args = (3141,)
+
+        self.check(*args)
+
+    def test_simple_list(self):
+        """
+        Try making a Derek tree from a simple list (depth 1),
+        without specifying the name of the root node.
+        """
+        # TODO: monkey testing
+
+        args = ([1,2,3],)
+
+        self.check(*args)
+
+    def test_list_in_list(self):
+        """
+        Try making a Derek tree from a list in list (depth 2),
+        without specifying the name of the root node.
+        """
+        # TODO: monkey testing
+
+        args = ([
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+        ],)
+
+        self.check(*args)
+
+    def test_list_in_dict(self):
+        """
+        Try making a Derek tree from a list in dict (depth 2),
+        without specifying the name of the root node.
+        """
+        # TODO: monkey testing
+
+        args = ({
+            'a': [1,2,3],
+            'b': [4,5,6],
+            'c': [7,8,9]
+        },)
+
+        self.check(*args)
+
+    def test_simple_dict(self):
+        """
+        Try making a Derek tree from a simple list (depth 1),
+        without specifying the name of the root node.
+        """
+        # TODO: monkey testing
+
+        args = ({
+            'a': 1,
+            'b': 2,
+            'c': 3
+        },)
+
+        self.check(*args)
+
+    def test_dict_in_dict(self):
+        """
+        Try making a Derek tree from a dict in dict (depth 2),
+        without specifying the name of the root node.
+        """
+        # TODO: monkey testing
+
+        args = ({
+            'a': {
+                'd': 1,
+                'e': 2,
+                'f': 3
+            },
+            'b': {
+                'g': 4,
+                'h': 5,
+                'i': 6
+            },
+            'c': {
+                'j': 7,
+                'k': 8,
+                'l': 9
+            }
+        },)
+
+        self.check(*args)
