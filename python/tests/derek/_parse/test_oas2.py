dd40050435e0eb78147d2a21192ba4a88275267c
Benjamin Woods <ben@bjqw.me>
2022-06-04 17:11:39 +0100

    Add more oas2 tests

--- python/tests/derek/_parse/test_oas2.py
+++ python/tests/derek/_parse/test_oas2.py
@@ -112,3 +112,165 @@ def test__unique_schemas(schemas):
             "type": "array",
         },
     ]
+
+
+class Test__oas2_dict:
+    @pytest.fixture(scope="class")
+    def node(self):
+        obj = {"a": 1, "b": "b1", "c": 3.0}
+
+        return Derek.tree(obj)
+
+    def test_permissive(self, node):
+        result = _oas2._oas2_dict(node, strategy="permissive")
+
+        assert result == {
+            "type": "object",
+            "additionalProperties": {
+                "oneOf": [
+                    {"type": "integer"},
+                    {"type": "string"},
+                    {"type": "number"},
+                ]
+            },
+        }
+
+    def test_restricted(self, node):
+        result = _oas2._oas2_dict(node, strategy="restricted")
+
+        assert result == {
+            "type": "object",
+            "properties": {
+                "a": {"type": "integer"},
+                "b": {"type": "string"},
+                "c": {"type": "number"},
+            },
+        }
+
+    def test_inner_join(self, node):
+        result = _oas2._oas2_dict(node, strategy="inner_join")
+
+        assert result == {
+            "type": "object",
+            "properties": {
+                "a": {"type": "integer"},
+                "b": {"type": "string"},
+                "c": {"type": "number"},
+            },
+        }
+
+
+class Test__oas2_list:
+    @pytest.fixture(scope="class")
+    def node(self):
+        obj = [{"a": 1, "b": "b1", "c": 3.0}, {"a": 4, "b": ["b2"]}, [1, 2, 3.0]]
+
+        return Derek.tree(obj)
+
+    def test_permissive(self, node):
+        result = _oas2._oas2_list(node, strategy="permissive")
+
+        assert result == {
+            "type": "array",
+            "items": {
+                "oneOf": [
+                    {
+                        "type": "object",
+                        "additionalProperties": {
+                            "oneOf": [
+                                {"type": "integer"},
+                                {"type": "string"},
+                                {"type": "number"},
+                            ]
+                        },
+                    },
+                    {
+                        "type": "object",
+                        "additionalProperties": {
+                            "oneOf": [
+                                {"type": "integer"},
+                                {"type": "array", "items": {"type": "string"}},
+                            ]
+                        },
+                    },
+                    {
+                        "items": {
+                            "oneOf": [
+                                {"type": "integer"},
+                                {"type": "number"},
+                            ]
+                        },
+                        "type": "array",
+                    },
+                ]
+            },
+        }
+
+    def test_restricted(self, node):
+        result = _oas2._oas2_list(node, strategy="restricted")
+
+        assert result == {
+            "type": "array",
+            "items": {
+                "oneOf": [
+                    {
+                        "type": "object",
+                        "properties": {
+                            "a": {"type": "integer"},
+                            "b": {"type": "string"},
+                            "c": {"type": "number"},
+                        },
+                    },
+                    {
+                        "type": "object",
+                        "properties": {
+                            "a": {"type": "integer"},
+                            "b": {"type": "array", "items": {"type": "string"}},
+                        },
+                    },
+                    {
+                        "items": {
+                            "oneOf": [
+                                {"type": "integer"},
+                                {"type": "number"},
+                            ]
+                        },
+                        "type": "array",
+                    },
+                ]
+            },
+        }
+
+    def test_inner_join(self, node):
+        result = _oas2._oas2_list(node, strategy="inner_join")
+
+        assert result == {
+            "type": "array",
+            "items": {
+                "oneOf": [
+                    {
+                        "type": "object",
+                        "properties": {
+                            "a": {"type": "integer"},
+                            "b": {
+                                "oneOf": [
+                                    {"type": "string"},
+                                    {"type": "array", "items": {"type": "string"}},
+                                ]
+                            },
+                            "c": {"type": "number"},
+                        },
+                        "required": ["a", "b"],
+                    },
+                    {
+                        "items": {
+                            "oneOf": [
+                                {"type": "integer"},
+                                {"type": "number"},
+                            ]
+                        },
+                        "type": "array",
+                    },
+                ]
+            },
+        }
